ffc9cb37bb3ecd42cd1044e54335764867ad94be
adelopo <aa.adelopo@gmail.com>
2026-06-05 17:38:40 +0100

    fix: remove ttl from livekit token

--- app/api/livekit/route.ts
+++ app/api/livekit/route.ts
@@ -74,7 +74,6 @@ export async function GET(req: NextRequest) {
         const at = new AccessToken(apiKey, apiSecret, {
             identity: identity!,
             name: name,
-            ttl: 4 * 60 * 60, // 4 hours max validity per token
         });
 
         at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
